4470f91053303415c2787e5c4d247798a61a5c87
wuhibe <wuhibetamire710@gmail.com>
2022-01-31 01:12:15 +0300

    formatting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-'''
-script that starts a Flask web application
-'''
+"""
+starts a Flask web application
+"""
 
-from flask import Flask, escape, request
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    ''' function to display Hello HBNB '''
-    return f'Hello HBNB!'
-
+def index():
+    """function to return Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
